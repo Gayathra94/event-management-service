dc269a362318cadcb86863236489c308e8b2c734
Gayathra94 <ranawakamadubashana@gmail.com>
2025-05-22 15:06:42 +0530

    Initial commit

---
+++ src/main/java/com/event/service/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.event.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+    @GetMapping(value = "/testAuth")
+    public String testAuth(){
+        return "Application is running..";
+    }
+
+}
